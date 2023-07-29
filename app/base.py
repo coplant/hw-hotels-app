29376e506c1fd4cb6813af3cd6a64becd3584eab
Ivan <omg.dayz@gmail.com>
2023-07-29 15:27:34 +0700

    add rooms model

--- app/base.py
+++ app/base.py
@@ -1,2 +1,3 @@
 from app.database import Base
-from app.hotels.models import Hotels
+
+from app.hotels import models
--- app/hotels/models.py
+++ app/hotels/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, JSON
+from sqlalchemy import Column, Integer, String, JSON, ForeignKey
 
 from app.database import Base
 
@@ -10,4 +10,16 @@ class Hotels(Base):
     location: str = Column(String, nullable=False)
     services: dict = Column(JSON)
     rooms_quantity: int = Column(Integer, nullable=False)
+    image_id: int = Column(Integer)
+
+
+class Rooms(Base):
+    __tablename__ = "rooms"
+    id: int = Column(Integer, primary_key=True)
+    hotel_id: int = Column(ForeignKey("hotels.id"))
+    name: str = Column(String, nullable=False)
+    description: str = Column(String)
+    price: int = Column(Integer, nullable=False)
+    services: dict = Column(JSON)
+    quantity: int = Column(Integer, nullable=False)
     image_id: int = Column(Integer)
