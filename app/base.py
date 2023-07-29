ae62e0a33827798f8aedb10d517dc9c9e443cbd4
Ivan <omg.dayz@gmail.com>
2023-07-29 15:28:15 +0700

    add bookings model

--- app/base.py
+++ app/base.py
@@ -2,3 +2,4 @@ from app.database import Base
 
 from app.users import models
 from app.hotels import models
+from app.bookings import models
---
+++ app/bookings/models.py
@@ -0,0 +1,17 @@
+from datetime import date
+
+from sqlalchemy import Integer, Column, Date, ForeignKey, Computed
+
+from app.database import Base
+
+
+class Bookings(Base):
+    __tablename__ = "bookings"
+    id: int = Column(Integer, primary_key=True)
+    room_id: int = Column(ForeignKey("rooms.id"))
+    user_id: int = Column(ForeignKey("users.id"))
+    date_from: date = Column(Date, nullable=False)
+    date_to: date = Column(Date, nullable=False)
+    price: int = Column(Integer, nullable=False)
+    total_cost: int = Column(Integer, Computed("(date_to - date_from) * price"))
+    total_days: int = Column(Integer, Computed("date_to - date_from"))
