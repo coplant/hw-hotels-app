4234635caec97a46b8366ed6b28b33040994e469
Ivan <omg.dayz@gmail.com>
2023-07-29 15:02:54 +0700

    add database connection

---
+++ app/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from app.config import settings
+
+engine = create_async_engine(settings.db.url)
+
+async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
