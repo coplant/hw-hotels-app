4708130ce9c795a66e00dd060ea4316f286bde0d
Ivan <omg.dayz@gmail.com>
2023-08-09 23:35:51 +0700

    add auth utils

--- app/users/auth.py
+++ app/users/auth.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timedelta
 from passlib.context import CryptContext
+from jose import jwt
+
+from app.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -9,3 +13,14 @@ def verify_password(plain_password, hashed_password):
 
 def get_hashed_password(password):
     return pwd_context.hash(password)
+
+
+def create_access_token(data: dict, expires_delta: timedelta = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=settings.security.access_token_expire_minutes)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, settings.security.jwt_secret_key, settings.security.jwt_algorithm)
+    return encoded_jwt
