6a4d99b0f338f0793cc9750a4c77ab4d71188b0a
Ivan <omg.dayz@gmail.com>
2023-08-19 19:04:00 +0700

    add bookings find_free_by_id

--- app/bookings/services.py
+++ app/bookings/services.py
@@ -2,7 +2,7 @@ from sqlalchemy import select, or_, and_, func, between, insert
 
 from app.bookings.models import Bookings
 from app.database import async_session_maker
-from app.hotels.models import Rooms
+from app.hotels.rooms.models import Rooms
 from app.services.base import BaseService
 
 
@@ -10,35 +10,41 @@ class BookingService(BaseService):
     model = Bookings
 
     @classmethod
-    async def add(cls, user_id, room_id, date_from, date_to):
+    async def find_free_by_id(cls, room_id, date_from, date_to):
         async with async_session_maker() as session:
             booked_rooms = select(func.count()).where(
                 and_(
-                    Bookings.room_id == room_id,
+                    cls.model.room_id == room_id,
                     or_(
-                        between(date_to, Bookings.date_from, Bookings.date_to),
-                        between(date_from, Bookings.date_from, Bookings.date_to),
-                        between(Bookings.date_from, date_from, date_to),
-                        between(Bookings.date_to, date_from, date_to)
+                        between(date_to, cls.model.date_from, cls.model.date_to),
+                        between(date_from, cls.model.date_from, cls.model.date_to),
+                        between(cls.model.date_from, date_from, date_to),
+                        between(cls.model.date_to, date_from, date_to)
                     )
                 )
             ).as_scalar()
-            get_rooms_left = select(func.coalesce(Rooms.quantity - booked_rooms, Rooms.quantity))
-
+            get_rooms_left = select(
+                func.coalesce(Rooms.quantity - booked_rooms, Rooms.quantity)).where(
+                Rooms.id == room_id
+            )
             result = await session.execute(get_rooms_left)
-            rooms_left: int = result.scalar()
+            return result.scalar()
 
+    @classmethod
+    async def add(cls, user_id, room_id, date_from, date_to):
+        rooms_left: int = await BookingService.find_free_by_id(room_id, date_from, date_to)
+        async with async_session_maker() as session:
             if rooms_left > 0:
                 get_price = select(Rooms.price).filter_by(id=room_id)
                 result = await session.execute(get_price)
                 price: int = result.scalar()
-                add_booking = insert(Bookings).values(
+                add_booking = insert(cls.model).values(
                     room_id=room_id,
                     user_id=user_id,
                     date_from=date_from,
                     date_to=date_to,
                     price=price
-                ).returning(Bookings)
+                ).returning(cls.model)
                 new_booking = await session.execute(add_booking)
                 await session.commit()
                 return new_booking.scalar()
