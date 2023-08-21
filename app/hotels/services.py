c34fb2cfd90fa813a590e57907511c291f23746f
Ivan <omg.dayz@gmail.com>
2023-08-21 19:25:00 +0700

    add find_by_location() service to hotels

--- app/hotels/services.py
+++ app/hotels/services.py
@@ -1,6 +1,17 @@
+from sqlalchemy import select
+from app.database import async_session_maker
 from app.hotels.models import Hotels
+from app.hotels.rooms.services import RoomService
+from app.hotels.schemas import SFreeHotels
 from app.services.base import BaseService
 
 
 class HotelService(BaseService):
     model = Hotels
+
+    @classmethod
+    async def find_by_location(cls, location: str):
+        async with async_session_maker() as session:
+            query = select(Hotels).filter(Hotels.location.icontains(location))
+            result = await session.execute(query)
+            return result.scalars().all()
