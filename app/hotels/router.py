6d753069fd52a4c4857bb5b149c921470672d799
Ivan <omg.dayz@gmail.com>
2023-08-22 20:50:00 +0700

    add hotels /location

--- app/hotels/router.py
+++ app/hotels/router.py
@@ -1,5 +1,8 @@
+from datetime import date
+
 from fastapi import APIRouter
 
+from app.hotels.schemas import SFreeHotels
 from app.hotels.services import HotelService
 
 router = APIRouter(prefix="/hotels", tags=["Hotels"])
@@ -8,3 +11,10 @@ router = APIRouter(prefix="/hotels", tags=["Hotels"])
 @router.get("")
 async def get_hotels():
     return await HotelService.find_all()
+
+
+@router.get("/{location}")
+async def get_hotels_by_location(
+        location: str, date_from: date, date_to: date
+) -> list[SFreeHotels]:
+    return await HotelService.find_free_by_location(location, date_from, date_to)
