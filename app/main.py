582a8a1b544595cab135e72fcf5ec212444621fe
Ivan <omg.dayz@gmail.com>
2023-08-23 19:44:00 +0700

    refactor app/main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 
-from app.hotels.router import router as router_hotels
 from app.bookings.router import router as router_bookings
+from app.hotels.router import router as router_hotels
 from app.users.router import router as router_users
 
 app = FastAPI()
